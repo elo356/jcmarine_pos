436f030374396b235d5ffc61c2a1a0dcefce13d5
elo356 <elian.0617@gmail.com>
2026-05-08 09:37:19 -0400

    tes

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -929,6 +929,8 @@ const StorePage = () => {
               </div>
             </div>
 
+            
+
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
               <div>
                 <Input
